fe72b1f1adac0f3df340a850dc8ff3ab5713f1ca
rmahotka <rmahotka@gmail.com>
2024-06-16 14:51:59 +0300

    small pravki

---
+++ components/HeaderMenu/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <NuxtLink to="/" class="logo-adaptive">
+        <iconsLogo />
+      </NuxtLink>
+      <div class="head" :style="{ right: burgetActive ? '0' : '' }">
+        <div class="header__block">
+          <NuxtLink to="/" class="logo-basic">
+            <iconsLogo />
+          </NuxtLink>
+          <nav class="menu__link-block">
+            <NuxtLink to="/tariffs" class="menu__link">Тарифы</NuxtLink>
+            <NuxtLink to="/contacts" class="menu__link">Контакты</NuxtLink>
+          </nav>
+        </div>
+        <div class="header__block opacity-menu">
+          <a class="header-phone" href="tel: +7 495 118-44-22">
+            +7 495 118-44-22
+          </a>
+          <NuxtLink class="menu__link" to="/login">Вход</NuxtLink>
+          <NuxtLink to="/register">
+            <UIButton :size="'small'"> Регистрация </UIButton>
+          </NuxtLink>
+        </div>
+      </div>
+      <div
+        class="burger"
+        :class="{ 'burger-active': burgetActive }"
+        @click="openBurger"
+      >
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const burgetActive = ref(false);
+
+const openBurger = () => {
+  burgetActive.value = !burgetActive.value;
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
